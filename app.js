df0bfd1af689541d00871cce16492475d2601b29
Asadullah Khan <asadullah178100@gmail.com>
2024-04-30 09:02:30 -0800

    done

---
+++ app.js
@@ -0,0 +1,7 @@
+
+
+
+const hour = document.querySelector('#hour');
+const minute = document.querySelector('#minute');
+const second = document.querySelector('#second');
+const startBtn = document.querySelector('#startBtn');
